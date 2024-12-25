e735215dca9a077c81d99b60ef5be94d1a8daee5
muham <muham@DESKTOP-7BQA3UJ>
2024-12-26 01:33:49 +0500

    Add project files.

---
+++ MyWebApp/Models/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace MyWebApp.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public List<ItemClient>? ItemClients { get; set; }
+    }
+}
